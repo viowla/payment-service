60398001bbdcf952ab94c0986a81f25a3ee89451
viowla <55015538+viowla@users.noreply.github.com>
2020-11-16 17:30:00 +0600

    add Service-to-service request

--- src/main/java/kz/iitu/paymentservice/TicketService.java
+++ src/main/java/kz/iitu/paymentservice/TicketService.java
@@ -3,10 +3,17 @@ package kz.iitu.paymentservice;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Component
@@ -21,4 +28,32 @@ public class TicketService {
         return ticket;
     }
 
+    @HystrixCommand(
+            fallbackMethod = "getUserTicketFallback",
+            threadPoolKey = "getUserTicket",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "100"),
+                    @HystrixProperty(name = "maxQueueSize", value = "50")
+            }
+    )
+    public UserTicket getUserTicket(String userId){
+        String apiCredentials = "rest-client:p@ssword";
+        String base64Credentials = new String(Base64.encodeBase64(apiCredentials.getBytes()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Credentials);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange("http://payment-info-service/ticket/info/" + userId,
+                HttpMethod.GET, entity, UserTicket.class).getBody();
+    }
+
+    public UserTicket getUserMovieFallback(String movieId) {
+        UserTicket userMovie = new UserTicket();
+        List<Ticket> list = new ArrayList<>();
+        list.add(new Ticket("-1", 0.0));
+        userMovie.setUserTickets(list);
+
+        return userMovie;
+    }
 }
